b6484c2dcda3918983933c5c45163ef4932a9284
reddification <reddification@gmail.com>
2017-04-01 01:05:30 +0500

    censorship

--- Templates/twitter_followers.php
+++ Templates/twitter_followers.php
@@ -1,23 +1,22 @@
 
-
 <p>
-    <span class="label label-default"> На вас подписаны вот эти ебланы </span>
+    <span class="label label-default"> На вас подписаны вот эти славные ребята </span>
     <span class="label label-primary"><?= count($twitterUser->followers) ?> </span>
 </p>
 <hr/>
 
-<?php foreach($twitterUser->followers as $eblan):?>
+<?php foreach($twitterUser->followers as $follower):?>
     <div class="twitter_user">
         <img class="img-responsive img-rounded"
-             src="<?= str_replace("_normal.",".",$eblan["profile_image_url"]) ?>" />
-        <a href="https://twitter.com/<?=$eblan["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $eblan["screen_name"]?></span> </a>
+             src="<?= str_replace("_normal.",".",$follower["profile_image_url"]) ?>" />
+        <a href="https://twitter.com/<?=$follower["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $follower["screen_name"]?></span> </a>
         <button type="button" class="btn btn-warning" data-toggle="tooltip" title="Забанить у*бка"
-                onclick="ban_faggot(<?= $eblan["id_str"] ?>, '<?= $eblan["screen_name"]?>') id="bitch_<?=$eblan["screen_name"]?>">
+                onclick="ban_faggot(<?= $follower["id_str"] ?>, '<?= $follower["screen_name"]?>') id="bitch_<?=$follower["screen_name"]?>">
             /ban
         </button>
         <blockquote>
-            <p><?= $eblan["description"]?></p>
-            <footer><?=$eblan["screen_name"]?></footer>
+            <p><?= $follower["description"]?></p>
+            <footer><?=$follower["screen_name"]?></footer>
         </blockquote>
     </div>
 <?php endforeach; ?>
