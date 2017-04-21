4d2e6bf4f7c11647d92247f5489d220454f79935
reddification <reddification@gmail.com>
2017-04-21 18:59:29 +0500

    Create twitter_followed.php
    это рэпчина не для проституток

--- Templates/twitter_followed.php
+++ Templates/twitter_followed.php
@@ -1,18 +1,18 @@
 <p>
-    <span class="label label-default"> Вы подписаны на вот этих славных ребят </span>
+    <span class="label label-default"> Вы подписаны на вот этих ебланы </span>
     <span class="label label-primary"><?= count($twitterUser->followed) ?> </span>
 </p>
 <hr/>
 
-<?php foreach($twitterUser->followed as $followed):?>
+<?php foreach($twitterUser->followed as $eblan):?>
     <div class="twitter_user">
         <img class="img-responsive img-rounded"
-             src="<?= str_replace("_normal.",".",$followed["profile_image_url"]) ?>" />
-        <a href="https://twitter.com/<?=$followed["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $followed["screen_name"]?></span> </a>
+             src="<?= str_replace("_normal.",".",$eblan["profile_image_url"]) ?>" />
+        <a href="https://twitter.com/<?=$eblan["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $eblan["screen_name"]?></span> </a>
 
         <blockquote>
-            <p><?= $followed["description"]?></p>
-            <footer><?=$followed["screen_name"]?></footer>
+            <p><?= $eblan["description"]?></p>
+            <footer><?=$eblan["screen_name"]?></footer>
         </blockquote>
     </div>
 <?php endforeach; ?>
