e064e25998804127642abc81555460d14c9fb658
reddification <reddification@gmail.com>
2017-04-21 18:58:32 +0500

    Update twitter_advices.php
    Всем бодрого времени суток

--- Templates/twitter_advices.php
+++ Templates/twitter_advices.php
@@ -1,23 +1,23 @@
 <p>
-    <span class="label label-default"> А ещё можешь подписаться на вот этих славных ребят: </span>
+    <span class="label label-default"> А ещё можешь подписаться на вот этих уродов: </span>
     <span class="label label-primary"><?= count($twitterUser->advices) ?> </span>
 </p>
 <hr/>
 
-<?php foreach($twitterUser->advices as $advice):?>
+<?php foreach($twitterUser->advices as $eblan):?>
     <div class="twitter_user">
         <img class="img-responsive img-rounded"
-             src="<?= str_replace("_normal.",".",$advice["profile_image_url"]) ?>" />
-        <a href="https://twitter.com/<?=$advice["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $advice["screen_name"]?></span> </a>
+             src="<?= str_replace("_normal.",".",$eblan["profile_image_url"]) ?>" />
+        <a href="https://twitter.com/<?=$eblan["screen_name"] ?>" target="_blank"> <span class="label label-default"><?= $eblan["screen_name"]?></span> </a>
 
         <button type="button" class="btn btn-primary" data-toggle="tooltip" title="дизлайк атписка"
-                onclick="subscribe_faggot(<?= $advice["id_str"]?>,'<?=$advice["screen_name"]?>')" id="bitch_<?=$advice["screen_name"]?>">
+                onclick="subscribe_faggot(<?= $eblan["id_str"]?>,'<?=$eblan["screen_name"]?>')" id="bitch_<?=$eblan["screen_name"]?>">
             падписацца
         </button>
 
         <blockquote>
-            <p><?= $advice["description"]?></p>
-            <footer><?=$advice["screen_name"]?></footer>
+            <p><?= $eblan["description"]?></p>
+            <footer><?=$eblan["screen_name"]?></footer>
         </blockquote>
     </div>
 <?php endforeach; ?>
